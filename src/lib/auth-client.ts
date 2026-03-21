73314d089d728336fd4f77de3d8ea96adcf9f0eb
powiedl <rolandpi@gmx.at>
2026-03-21 13:17:29 +0100

    project url updated

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -4,6 +4,6 @@ export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
   baseURL:
     process.env.NODE_ENV === 'production'
-      ? 'https://recall-tanstack-start-tutorial.vercel.app'
+      ? 'https://udemy-notes.vercel.app/'
       : 'http://localhost:3000',
 })
