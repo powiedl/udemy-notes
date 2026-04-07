c53f016ec3d546b36612f0bbd3ba120a927e6255
powiedl <rolandpi@gmx.at>
2026-04-07 04:58:21 +0200

    deleteTag wrapped in try catch

--- src/components/web/tag.tsx
+++ src/components/web/tag.tsx
@@ -1,11 +1,12 @@
 import { deleteTagFn, getAvailableTagsFn } from '#/data/tag'
-import { ServerFnData } from '#/types/api'
+import { ClientLoggingMetadata, ServerFnData } from '#/types/api'
 import { X } from 'lucide-react'
 import { Badge } from '../ui/badge'
 import { useServerFn } from '@tanstack/react-start'
 import { useTransition } from 'react'
 import { Button } from '../ui/button'
 import { cn } from '#/lib/utils'
+import { toast } from 'sonner'
 
 const Tag = ({
   tag,
@@ -14,9 +15,24 @@ const Tag = ({
 }) => {
   const deleteTag = useServerFn(deleteTagFn)
   const [isDeleting, startDeleteTransition] = useTransition()
+
+  // Logging information für diese Komponente
+  const loggingMetadata: ClientLoggingMetadata = {
+    component: 'Tag', // Der Name der Komponente
+    feature: 'DeleteTag', // Optional: Spezifische Aktion
+  }
   const handleDeleteTag = async (id: string) => {
     startDeleteTransition(async () => {
-      await deleteTag({ data: { id } })
+      try {
+        await deleteTag({ data: { id, loggingMetadata } })
+      } catch (error) {
+        if (error instanceof Error) toast.error(error.message)
+        else if (typeof error === 'string') toast.error(error)
+        else
+          toast.error(
+            'Something unexpected happened while trying to delete the tag',
+          )
+      }
     })
   }
   return (
