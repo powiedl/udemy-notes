77460731a7f6a4bb50da9a154098511ee8173463
powiedl <rolandpi@gmx.at>
2026-05-10 17:50:55 +0200

    implemented strict import chain note.schema - note.logic - note file

---
+++ src/schemas/note.schema.ts
@@ -0,0 +1,24 @@
+import { z } from 'zod'
+import { withLogging } from './api-utils'
+import { courseNotesSearchSchema, noteSearchSchema } from './search-params'
+
+// #region validation schemas
+export const getNotesSchema = withLogging(noteSearchSchema)
+export const toggleNoteTagSchema = withLogging(
+  z.object({
+    noteId: z.string(),
+    tagId: z.string(),
+    action: z.enum(['add', 'remove']),
+  }),
+)
+export const getNotesForCourseInputSchema = withLogging(
+  z.object({ courseId: z.string(), searchParams: courseNotesSearchSchema }),
+)
+
+export const updateNoteContentSchema = withLogging(
+  z.object({ noteId: z.string(), content: z.string() }),
+)
+// #endregion
+
+// #region types
+// #endregion
